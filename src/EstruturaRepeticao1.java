223d8f59e134e241dd0830518ab31b77490b4d5d
Higo Souza <higoedu@gmail.com>
2023-01-08 18:48:08 -0300

    estrutura de repetição 1
    estrutura de repetição 1

---
+++ src/EstruturaRepeticao1.java
@@ -0,0 +1,15 @@
+/*
+loops - while
+*/
+public class EstruturaRepeticao1 {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        //enquanto (condição)
+        while(contador <= 5){
+            System.out.println(contador);
+            //vai incrementar mais 1
+            contador += 1;
+        }
+    }
+}
