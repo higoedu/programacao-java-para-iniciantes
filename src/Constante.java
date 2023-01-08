c31353bc5714fddea3eb214cdd18146434aebc17
Higo Souza <higoedu@gmail.com>
2023-01-08 16:59:01 -0300

    constante em java
    constante em java

---
+++ src/Constante.java
@@ -0,0 +1,10 @@
+public class Constante {
+    public static void main(String[] args) {
+        final int numero = 9;
+        final String nomeBanco = "db_fornecedor";
+
+        //constante o valor não pode ser modificado
+        System.out.println(numero);
+        System.out.println(nomeBanco);
+    }
+}
