f6c924421325990a43cb142e8889460b24132170
Higo Souza <higoedu@gmail.com>
2023-01-08 17:13:09 -0300

    operadores relacionais
    operadores relacionais

---
+++ src/OperadoresRelacionais.java
@@ -0,0 +1,21 @@
+/*
+igual ==
+menor <
+maior >
+menor ou igual <=
+maior ou igual >=
+diferente !=
+*/
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int valor1 = 20;
+        int valor2 = 10;
+
+        System.out.println(valor1 == valor2);
+        System.out.println(valor1 < valor2);
+        System.out.println(valor1 > valor2);
+        System.out.println(valor1 <= valor2);
+        System.out.println(valor1 >= valor2);
+        System.out.println(valor1 != valor2);
+    }
+}
